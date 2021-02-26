08f3267686b624fc5936f7010f92191c52fae209
JEANThibaut <thibaut-jean@live.fr>
2021-02-26 10:06:21 +0100

    PlayerChoice/ComputerRandomized

--- js/main.js
+++ js/main.js
@@ -7,6 +7,38 @@ alert("Bienvenue!");
 var person = prompt("Merci d'entrer votre nom");
 console.log("Bienvenue " + person);
 
-//----
+//----"Ready to play" message
 
 alert("Bonjour "+ person + ", prêt pour une partie?");
+
+
+//----Choices
+
+var choices=[
+
+    "pierre",
+    "feuille",
+    "ciseaux"
+];
+
+
+//-----player choice
+
+var playerChoice = prompt("Veuillez choisir entre: \n pierre \n feuille \n ciseaux") 
+console.log(playerChoice)
+
+//----computer choice
+
+var choices =[
+    "pierre",
+    "feuille",
+    "ciseaux"
+];
+
+var i= Math.floor(Math.random()*3);
+console.log(i);
+
+var computerChoice = choices[i];
+console.log("L'ordinateur a choisi " + computerChoice);
+
+ 
---
+++ js/random.js
@@ -0,0 +1,12 @@
+var choices =[
+    "pierre",
+    "feuille",
+    "ciseaux"
+];
+
+
+var i= Math.floor(Math.random()*3);
+console.log(i);
+
+var computerChoice = choices[i];
+console.log(computerChoice);
